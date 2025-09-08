8e35a532e1f3a6715cfc3069a4c86979dcfefd23
xdai555 <vip@xdai.vip>
2025-09-09 03:31:12 +0800

    feat: 添加Input和Tag组件，并挂载Message组件到Vue原型链

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueCodemirror from 'vue-codemirror'
-import { Container, Footer, Switch, Col, Main, Select, Option, Button, InputNumber, Tooltip, Alert, Dialog } from 'element-ui'
+import { Container, Footer, Switch, Col, Main, Select, Option, Button, InputNumber, Tooltip, Alert, Dialog, Input, Tag, Message } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 // import ElementUI from 'element-ui'
 
@@ -19,7 +19,11 @@ Vue.use(InputNumber)
 Vue.use(Tooltip)
 Vue.use(Alert)
 Vue.use(Dialog)
+Vue.use(Input)
+Vue.use(Tag)
 // Vue.use(ElementUI)
+
+Vue.prototype.$message = Message
 
 Vue.config.productionTip = false
 

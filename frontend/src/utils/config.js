0889266218fe8d6a4a71ccf816d34d9d211545e1
xdai555 <vip@xdai.vip>
2025-09-11 22:21:08 +0800

    update api url

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -2,5 +2,6 @@ export default {
 
   // API_BASE_URL: 'http://127.0.0.1:9999' /* 本地调试 */
   // API_BASE_URL: 'http://api.xdai.vip:9999' /* 线上环境 - 废弃 */
-  API_BASE_URL: 'https://denpgcrevfkw.ap-northeast-1.clawcloudrun.com' /* 免费环境，穷 */
+  // API_BASE_URL: 'https://denpgcrevfkw.ap-northeast-1.clawcloudrun.com' /* 免费环境，穷 */
+  API_BASE_URL: 'https://api1.xdai.vip' /* 免费环境，穷 */
 }

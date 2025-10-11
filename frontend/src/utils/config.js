f7dd6d0818bb42f658854ace66ffd5dc8a9d2f32
EricWu <vip@xdai.vip>
2025-10-11 15:30:01 +0800

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -2,6 +2,6 @@ export default {
 
   // API_BASE_URL: 'http://127.0.0.1:9999' /* 本地调试 */
   // API_BASE_URL: 'http://api.xdai.vip:9999' /* 线上环境 - 废弃 */
-  API_BASE_URL: 'https://ykvmhvnsboit.ap-northeast-1.clawcloudrun.com/' /* 免费环境，穷 */
+  API_BASE_URL: 'https://ykvmhvnsboit.ap-northeast-1.clawcloudrun.com' /* 免费环境，穷 */
   // API_BASE_URL: 'https://api2.xdai.vip'  /* 免费环境，暂时还不支持解析 CNAME。。 */
 }
